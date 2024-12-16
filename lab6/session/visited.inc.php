df4d911183b1e7ae38c7eb371d54f07fd4195789
DariSH19 <sh.dasha999@yandex.ru>
2024-12-16 10:00:37 +0300

    .

--- lab6/session/visited.inc.php
+++ lab6/session/visited.inc.php
@@ -11,10 +11,10 @@ declare(strict_types=1);
 echo "<h2>Список посещённых страниц</h2>";
 if (isset($_SESSION['visitedPages']) && count($_SESSION['visitedPages']) > 0) {
     
-	echo "<ul>";
+	echo "<ol>";
     foreach ($_SESSION['visitedPages'] as $page)
         echo "<li>$page</li>";
-    echo "</ul>";
+    echo "</ol>";
 	
 } else
     echo "<p>Вы пока не посетили ни одну страницу :(</p>";
